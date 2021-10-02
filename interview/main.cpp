61ac662d08265d954d9eb3dc34b7c76224a752ab
swapnil <bhalerao.swapnil11@gmail.com>
2021-10-02 07:01:08 +0530

    Initial Commit

---
+++ interview/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  interview
+//
+//  Created by Swapnil Bhalerao on 02/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
